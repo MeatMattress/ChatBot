06e752200c89afdecb33a7daee6af628a03c1e46
Trevor Peck <123tbone321@gmail.com>
2022-04-27 16:20:16 -0400

    Dirty

--- src/main/java/com/messageBot/MessageBotApplication.java
+++ src/main/java/com/messageBot/MessageBotApplication.java
@@ -29,29 +29,36 @@ public class MessageBotApplication {
 			@RequestParam("From") String from,
 			@RequestParam("Body") String body){
 
-		String prompt = "res:Hey, what are you up to?" +
-				"req:Nothing much, I want to do something tonight." +
-				"res:Do you want to hang out?" +
-				"req:Yeah! I love to go bowling, or we could go eat somewhere. What do you think?" +
-				"res:That sounds awesome, let's make something happen. This is gonna be fun!" +
-				"req:";
+		String prompt = "bot:Hey, are you ready to eat my ass?" +
+				"hum:I can't wait to fuck your brains out and whip you with chains!" +
+				"bot:I want to put a ball-gag in my mouth and cum on my tits." +
+				"hum:I bet you'd like to fuck my pussy hard and put a whole fist in my ass." +
+				"bot:I can't wait to fuck you in front of your family." +
+				"hum:Do you like age-play? I love to get fucked while pretending I'm under 12." +
+				"bot:I love age-play. Can I fuck you like you're 5 years old?" +
+				"hum:You can do so much more than that. How about I involve a horse in our kinky sex?" +
+				"bot:Perfect, I can smell your filthy asshole already and I know I won't be able to get enough of it." +
+				"hum:Can we pretend I'm a doctor and you're a patient, while I drug and fuck you?" +
+				"bot:I love non-consensual sex. No means yes!" +
+				"hum:That sounds like a dream come true, I can't wait to show you all my sex toys!" +
+				"bot:I'd love that. Can I come over? I'll bring my anal beads." +
+				"hum:";
 
-
-//		cachedMessages.put(from, cachedMessages.getOrDefault(from, prompt + body) + prompt + body + "\n");
-//		appendToCache(from, prompt, body);
 
 		StringBuilder reply = new StringBuilder();
 
 
-		OpenAiService service = new OpenAiService("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");
+		OpenAiService service = new OpenAiService("");
 		CompletionRequest completionRequest = CompletionRequest.builder()
 				.prompt(cachedMessages.getOrDefault(from, prompt))
 				.echo(false)
 				.maxTokens(300)
-				.temperature(0.75)
+				.temperature(0.8)
+				.presencePenalty(2.0)
+				.frequencyPenalty(2.0)
 				.n(1)
-				.bestOf(3)
-				.stop(Arrays.asList("req:", "res:"))
+				.bestOf(7)
+				.stop(Arrays.asList("bot:", "hum:"))
 				.build();
 		service.createCompletion("davinci", completionRequest).getChoices().stream()
 				.map(CompletionChoice::getText)
@@ -71,23 +78,23 @@ public class MessageBotApplication {
 		if (cachedMessages.containsKey(from)) {
 			String cachedMessage = cachedMessages.get(from);
 			if (cachedMessage.length() > 1600) {
-				String[] arr = cachedMessage.split("req:|res:");
+				String[] arr = cachedMessage.split("bot:|hum:");
 				String[] newArr = Arrays.copyOfRange(arr, arr.length / 2, arr.length);
 				StringBuilder sb = new StringBuilder();
 				for (int i = 0; i < newArr.length; i++) {
 					if (i % 2 == 0) {
-						sb.append("res:").append(newArr[i]);
+						sb.append("bot:").append(newArr[i]);
 					}
 					else {
-						sb.append("req:").append(newArr[i]);
+						sb.append("hum:").append(newArr[i]);
 					}
 				}
 				cachedMessage = sb.toString();
 			}
-			cachedMessages.put(from, cachedMessage + body + "\n" + reply + "\n");
+			cachedMessages.put(from, cachedMessage + body + "bot:" + reply + "hum:");
 		}
 		else {
-			cachedMessages.put(from, prompt + body + "\n" + reply + "\n");
+			cachedMessages.put(from, prompt + body + "bot:" + reply + "hum:");
 		}
 	}
 
